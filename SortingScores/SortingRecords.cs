bd22f1f621e85de4b6fed8b2794bdcbbeed659b2
Toni Lam <qut.toni.9516778@gmail.com>
2017-04-09 22:54:12 +1000

    Verify the parameter and alert for error

---
+++ SortingScores/SortingRecords.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingScores {
+    class SortingRecords {
+        static void Main(string[] args) {
+            if (args.Length == 1) {
+                // do something
+            } else {
+                alertErrorArgs();
+            }
+        }
+
+        static private void alertErrorArgs() {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("This application needs one parameter to specific"
+                              + " where the input text file is, e.g.");
+            Console.WriteLine("SortingScores <filename>");
+        }
+    }
+}
